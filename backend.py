768edcbbc0ff1175871e6414dc36459f1eb5beab
ruedibuechi <ruedi.buechi@postfinance.ch>
2023-01-04 20:42:17 +0100

    added plot for price history

--- backend.py
+++ backend.py
@@ -1,5 +1,9 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
 
-data = yf.download("AAPL NESN.SW BMW.DE TSLA NIO", start="2019-01-01", end="2022-12-01")
+data = yf.download("AAPL NESN.SW BMW.DE TSLA NIO", start="2019-01-01", end="2022-12-01")['Close']
 
-data.to_csv('shareprices.csv')
+print(data.head())
+
+data.plot()
+plt.show()
